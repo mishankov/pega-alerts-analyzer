033f2c089d9a32c3c5d6f1d082ef80ac849e2de0
mishankov <mishankov@mail.com>
2020-12-13 02:31:10 +0300

    multiple files upload

--- client/src/components/AlertsUpload.vue
+++ client/src/components/AlertsUpload.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container">
     <div class="large-12 medium-12 small-12 cell">
-      <label>File
-        <input type="file" id="file" ref="file" v-on:change="handleFileUpload()"/>
+      <label>Files
+        <input type="file" id="files" ref="files" multiple v-on:change="handleFilesUpload()"/>
       </label>
-      <button v-on:click="submitFile()">Submit</button>
+      <button v-on:click="submitFiles()">Submit</button>
     </div>
   </div>
 </template>
@@ -13,54 +13,68 @@
 import axios from 'axios';
 
 export default {
-/*
-    Defines the data used by the component
-*/
-  name: 'AlertsUpload',
+  /*
+      Defines the data used by the component
+    */
   data() {
     return {
-      file: '',
+      files: '',
     };
   },
 
   methods: {
     /*
-    Submits the file to the server
-    */
-    submitFile() {
-    /*
-            Initialize the form data
+        Submits all of the files to the server
+      */
+    submitFiles() {
+      /*
+          Initialize the form data
         */
-      const formData = new FormData();
+      // const formData = new FormData();
 
       /*
-            Add the form data we need to submit
+          Iteate over any file sent over appending the files
+          to the form data.
         */
-      formData.append('file', this.file);
+      for (let i = 0; i < this.files.length; i += 1) {
+        const formData = new FormData();
+        formData.append('file', this.files[i]);
+        axios.post('http://localhost:8000/alerts/uploadfile',
+          formData,
+          {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
+          }).then(() => {
+          console.log('SUCCESS!!');
+        })
+          .catch(() => {
+            console.log('FAILURE!!');
+          });
+      }
 
       /*
-        Make the request to the POST /single-file URL
-    */
-      axios.post('http://localhost:8000/alerts/uploadfile',
-        formData,
-        {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-        }).then(() => {
-        console.log('SUCCESS!!');
-      })
-        .catch(() => {
-          console.log('FAILURE!!');
-        });
+          Make the request to the POST /multiple-files URL
+        */
+      // axios.post('http://localhost:8000/alerts/uploadfile',
+      //   formData,
+      //   {
+      //     headers: {
+      //       'Content-Type': 'multipart/form-data',
+      //     },
+      //   }).then(() => {
+      //   console.log('SUCCESS!!');
+      // })
+      //   .catch(() => {
+      //     console.log('FAILURE!!');
+      //   });
     },
 
     /*
-    Handles a change on the file upload
-    */
-    handleFileUpload() {
-      const index = 0;
-      this.file = this.$refs.file.files[index];
+        Handles a change on the file upload
+      */
+    handleFilesUpload() {
+      this.files = this.$refs.files.files;
     },
   },
 };
